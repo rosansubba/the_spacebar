2fb13f1562d7bc1be46bfee6405dac8c01f3d2eb
Rosan Subba <rosansubba.43@gmail.com>
2021-12-02 20:58:18 +0545

    creating a page: Route and Controller

--- src/Controller/LuckeyController.php
+++ src/Controller/LuckeyController.php
@@ -15,4 +15,20 @@ class LuckeyController extends AbstractController
             'controller_name' => 'LuckeyController',
         ]);
     }
+
+    /**
+     * @Route("/lucky/number")
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+//        return new Response(
+//            '<html><body>Lucky number: '.$number.'</body></html>'
+//        );
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+
 }
